4c2a55e5a75788ed2d203b80d99bd033ffad9ac5
farazmanjhoo <farazmanjhoo0000@gmail.com>
2025-07-21 06:17:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-  from flask import Flask
+from flask import Flask
 
-  app = Flask(__name__)
+app = Flask(__name__)
 
-  @app.route('/')
-  def home():
-      return "<h1>BTC Dashboard Running Successfully 🚀</h1>"
+@app.route('/')
+def home():
+    return "<h1>BTC Dashboard Running Successfully 🚀</h1>"
 
-  if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=10000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
